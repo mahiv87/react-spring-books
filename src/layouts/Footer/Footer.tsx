1e3222ef9293deae86ff07e6a23dd6404933e90b
mahiv87 <maherreraiv1987@gmail.com>
2024-06-25 19:00:29 -0600

    centered Footer

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
 		<div className="main-color">
-			<div className="container flex flex-wrap justify-between items-center p-5 main-color">
+			<div className="container flex flex-wrap justify-between items-center mx-auto px-2 py-5 main-color">
 				<p className="md:w-1/3 mb-0 text-white">© React Spring Books, Inc</p>
 				<ul className="md:w-1/3 flex justify-end">
 					<li>
