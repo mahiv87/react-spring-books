5b9b4bd4da1b0be04f004cfd6ec2a4368675a230
mahiv87 <maherreraiv1987@gmail.com>
2024-06-25 19:28:07 -0600

    fetching books from db

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from 'react';
 import ReturnBook from './ReturnBook';
+import BookModel from '../../../models/BookModel';
 
 const Carousel = () => {
+	const [books, setBooks] = useState<BookModel[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [httpError, setHttpError] = useState(null);
+
+	useEffect(() => {
+		const fetchBooks = async () => {
+			const baseUrl: string = 'http://localhost:8080/api/books';
+			const url: string = `${baseUrl}?page=0size=9`;
+
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error('Something went wrong...');
+			}
+
+			const responseJson = await response.json();
+			const responseData = responseJson._embedded.books;
+
+			const loadedBooks = responseData.map((book: BookModel) => ({
+				id: book.id,
+				title: book.title,
+				author: book.author,
+				description: book.description,
+				copies: book.copies,
+				copiesAvailable: book.copiesAvailable,
+				category: book.category,
+				img: book.img
+			}));
+
+			setBooks(loadedBooks);
+			setIsLoading(false);
+		};
+		fetchBooks().catch((error: any) => {
+			setIsLoading(false);
+			setHttpError(error.message);
+		});
+	}, []);
+
 	return (
 		<div
 			className="container flex flex-col w-full mx-auto justify-start items-center"
