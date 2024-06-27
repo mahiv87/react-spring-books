d4540466d1a5471d2863994c9290590f9b2855cf
mahiv87 <maherreraiv1987@gmail.com>
2024-06-26 19:41:14 -0600

    Search bar layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './layouts/Navbar/Navbar';
 import Homepage from './layouts/HomePage/Homepage';
 import Footer from './layouts/Footer/Footer';
+import SearchBooks from './layouts/SearchBooks/SearchBooks';
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			<Homepage />
+			{/* <Homepage /> */}
+			<SearchBooks />
 			<Footer />
 		</>
 	);
--- src/layouts/SearchBooks/SearchBooks.tsx
+++ src/layouts/SearchBooks/SearchBooks.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import BookModel from '../../models/BookModel';
+import Spinner from '../utils/Spinner';
 
 const SearchBooks = () => {
 	const [books, setBooks] = useState<BookModel[]>([]);
@@ -9,7 +10,7 @@ const SearchBooks = () => {
 	useEffect(() => {
 		const fetchBooks = async () => {
 			const baseUrl: string = 'http://localhost:8080/api/books';
-			const url: string = `${baseUrl}?page=0size=9`;
+			const url: string = `${baseUrl}?page=0size=5`;
 
 			const response = await fetch(url);
 			if (!response.ok) {
@@ -38,10 +39,65 @@ const SearchBooks = () => {
 			setHttpError(error.message);
 		});
 	}, []);
+
+	if (isLoading) {
+		return <Spinner />;
+	}
+
+	if (httpError) {
+		return (
+			<div className="container mx-auto my-5" style={{ height: 550 }}>
+				<p>{httpError}</p>
+			</div>
+		);
+	}
+
 	return (
-		<>
-			<div>SearchBooks</div>
-		</>
+		<div>
+			<div className="container mx-auto my-5">
+				<div>
+					<div className="flex flex-wrap mt-5">
+						<div className="w-1/2">
+							<div className="flex">
+								<input
+									type="search"
+									className="form-input mr-2 bg-white border border-solid border-teal-500 rounded-lg pl-2"
+									placeholder="Search"
+									aria-labelledby="Search"
+								/>
+								<button className="py-3 px-6 text-lg border border-solid border-teal-500 rounded-lg hover:bg-teal-500 hover:text-white">
+									Search
+								</button>
+							</div>
+						</div>
+						<div className="w-1/3">
+							<details className="dropdown">
+								<summary className="py-3 px-6 text-lg border border-solid border-teal-500 rounded-lg list-none hover:bg-teal-500 hover:text-white hover:border-teal-500">
+									Category
+								</summary>
+								<ul className="menu dropdown-content bg-white rounded-lg z-[1] w-52 p-2 shadow">
+									<li>
+										<a>All</a>
+									</li>
+									<li>
+										<a>Front End</a>
+									</li>
+									<li>
+										<a>Back End</a>
+									</li>
+									<li>
+										<a>Data</a>
+									</li>
+									<li>
+										<a>DevOps</a>
+									</li>
+								</ul>
+							</details>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
 
