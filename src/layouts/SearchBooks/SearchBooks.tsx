c95666e036cb9850dc91d697fead4b2629d55e21
mahiv87 <maherreraiv1987@gmail.com>
2024-06-26 20:45:50 -0600

    book search component working now

--- src/layouts/SearchBooks/SearchBooks.tsx
+++ src/layouts/SearchBooks/SearchBooks.tsx
@@ -11,7 +11,7 @@ const SearchBooks = () => {
 	useEffect(() => {
 		const fetchBooks = async () => {
 			const baseUrl: string = 'http://localhost:8080/api/books';
-			const url: string = `${baseUrl}?page=0size=5`;
+			const url: string = `${baseUrl}?page=0&size=5`;
 
 			const response = await fetch(url);
 			if (!response.ok) {
@@ -57,26 +57,26 @@ const SearchBooks = () => {
 		<div>
 			<div className="container mx-auto my-5">
 				<div>
-					<div className="flex flex-wrap justify-center items-center mt-5">
+					<div className="flex flex-col md:flex-row justify-center items-center mt-5">
 						<div className="w-1/2">
-							<div className="flex">
+							<div className="flex flex-col md:flex-row justify-center items-center ">
 								<input
 									type="search"
-									className="form-input w-80 italic text-lg mr-2 bg-white border border-solid border-teal-500 rounded-lg pl-2"
+									className="form-input w-80 h-12 italic text-lg mr-2 bg-white border border-solid border-teal-500 rounded-lg pl-2"
 									placeholder="Search"
 									aria-labelledby="Search"
 								/>
-								<button className="btn bg-teal-500 text-white border-solid border-teal-500 rounded-lg hover:bg-teal-500/80 hover:border-teal-500 hover:text-white">
+								<button className="w-24 btn bg-teal-500 text-white border-solid border-teal-500 mt-2 md:mt-0 hover:bg-teal-500/80 hover:border-teal-500 hover:text-white">
 									Search
 								</button>
 							</div>
 						</div>
-						<div className="w-1/3 text-center">
+						<div className="md:w-1/3 text-center mt-2 md:mt-0">
 							<details className="dropdown">
-								<summary className="btn bg-teal-500 text-white border-solid border-teal-500 list-none hover:bg-teal-500/80 hover:text-white hover:border-teal-500">
+								<summary className="w-24 btn bg-teal-500 text-white border-solid border-teal-500 list-none hover:bg-teal-500/80 hover:text-white hover:border-teal-500">
 									Category
 								</summary>
-								<ul className="menu dropdown-content bg-white rounded-lg z-[1] w-52 p-2 shadow">
+								<ul className="menu dropdown-content bg-gray-100 rounded-lg z-[1] w-52 p-2 absolute left-1/2 transform -translate-x-1/2 shadow-lg">
 									<li>
 										<a>All</a>
 									</li>
--- src/layouts/SearchBooks/components/SearchBook.tsx
+++ src/layouts/SearchBooks/components/SearchBook.tsx
@@ -3,19 +3,22 @@ import defaultImg from '../../../Images/BooksImages/book-luv2code-1000.png';
 
 const SearchBook: React.FC<{ book: BookModel }> = ({ book }) => {
 	return (
-		<div className="card lg:card-side md:w-2/3 mx-auto my-5 shadow-xl">
-			<figure>
+		<div className="card lg:card-side md:w-2/3 md:mx-auto mx-5 my-5 shadow-xl">
+			<figure className="lg:w-1/3 lg:mx-auto">
 				{book.img ? (
 					<img src={book.img} width="123" height="196" alt="Book" />
 				) : (
 					<img src={defaultImg} width="123" height="196" alt="Book" />
 				)}
 			</figure>
-			<div className="card-body">
-				<h2 className="card-title">{book.title}</h2>
+			<div className="card-body lg:w-2/3 lg:mx-auto">
+				<h2 className="card-title font-bold text-2xl">{book.title}</h2>
+				<h4 className="text-lg italic">{book.author}</h4>
 				<p>{book.description}</p>
-				<div className="card-actions justify-end">
-					<button className="btn btn-primary text-white">View Details</button>
+				<div className="card-actions justify-center lg:justify-end">
+					<button className="btn bg-neutral-500 border-neutral-500 hover:bg-neutral-500/80 hover:border-neutral-500 text-white">
+						View Details
+					</button>
 				</div>
 			</div>
 		</div>
