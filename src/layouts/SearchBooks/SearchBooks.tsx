450c128a74c63b419de03641f6c4b5235096aada
mahiv87 <maherreraiv1987@gmail.com>
2024-06-27 18:44:40 -0600

    implementing pagination on searchbooks component

--- src/layouts/SearchBooks/SearchBooks.tsx
+++ src/layouts/SearchBooks/SearchBooks.tsx
@@ -8,11 +8,17 @@ const SearchBooks = () => {
 	const [books, setBooks] = useState<BookModel[]>([]);
 	const [isLoading, setIsLoading] = useState(true);
 	const [httpError, setHttpError] = useState(null);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [booksPerPage] = useState(5);
+	const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+	const [totalPages, setTotalPages] = useState(0);
 
 	useEffect(() => {
 		const fetchBooks = async () => {
 			const baseUrl: string = 'http://localhost:8080/api/books';
-			const url: string = `${baseUrl}?page=0&size=5`;
+			const url: string = `${baseUrl}?page=${
+				currentPage - 1
+			}&size=${booksPerPage}`;
 
 			const response = await fetch(url);
 			if (!response.ok) {
@@ -21,6 +27,9 @@ const SearchBooks = () => {
 
 			const responseJson = await response.json();
 			const responseData = responseJson._embedded.books;
+
+			setTotalAmountOfBooks(responseJson.page.totalElements);
+			setTotalPages(responseJson.page.totalPages);
 
 			const loadedBooks = responseData.map((book: BookModel) => ({
 				id: book.id,
@@ -53,6 +62,15 @@ const SearchBooks = () => {
 			</div>
 		);
 	}
+
+	const indexOfLastBook: number = currentPage * booksPerPage;
+	const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+	let lastItem =
+		booksPerPage * currentPage <= totalAmountOfBooks
+			? booksPerPage * currentPage
+			: totalAmountOfBooks;
+
+	const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
 	return (
 		<div>
