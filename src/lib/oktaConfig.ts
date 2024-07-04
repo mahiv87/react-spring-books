3493573d150e5881acb9d7e6189938f35ac73823
mahiv87 <maherreraiv1987@gmail.com>
2024-07-04 13:50:04 -0600

    Okta config'd

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
-	clientId: '',
-	issuer: '',
-	redirectUri: '',
+	clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+	issuer: process.env.REACT_APP_OKTA_ISSUER,
+	redirectUri: process.env.REACT_APP_OKTA_REDIRECT_URI,
 	scopes: ['openid', 'profile', 'email'],
 	pkce: true,
 	disableHttpsCheck: true
