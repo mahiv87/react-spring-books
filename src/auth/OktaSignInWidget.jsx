8a3ded997bd5f6642f81de188c266cd4991af06b
mahiv87 <maherreraiv1987@gmail.com>
2024-07-04 14:20:53 -0600

    okta sign in done

--- src/auth/OktaSignInWidget.jsx
+++ src/auth/OktaSignInWidget.jsx
@@ -1,5 +1,28 @@
-const OktaSignInWidget = () => {
-	return <div>OktaSignInWidget</div>;
+import OktaSignIn from '@okta/okta-signin-widget';
+import { useEffect, useRef } from 'react';
+import { oktaConfig } from '../lib/oktaConfig';
+
+const OktaSignInWidget = ({ onSuccess, onError }) => {
+	const widgetRef = useRef();
+
+	useEffect(() => {
+		if (!widgetRef.current) {
+			return false;
+		}
+
+		const widget = new OktaSignIn(oktaConfig);
+
+		widget
+			.showSignInToGetTokens({
+				el: widgetRef.current
+			})
+			.then(onSuccess)
+			.catch(onError);
+
+		return () => widget.remove();
+	}, [onSuccess, onError]);
+
+	return <div>{widgetRef}</div>;
 };
 
 export default OktaSignInWidget;
