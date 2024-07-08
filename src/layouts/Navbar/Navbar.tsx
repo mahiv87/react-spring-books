c8cd2d4afafe0a71a6727f3d426068870f732c46
mahiv87 <maherreraiv1987@gmail.com>
2024-07-08 09:15:36 -0600

    added nav link to users books to navbar

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -54,6 +54,16 @@ const Navbar = () => {
 									Search Books
 								</Link>
 							</li>
+							{authState.isAuthenticated && (
+								<li>
+									<Link
+										className="text-gray-100 transition hover:text-gray-100/75"
+										to="/shelf"
+									>
+										Your Books
+									</Link>
+								</li>
+							)}
 						</ul>
 					</nav>
 					<div className="flex items-center gap-4">
