301abfd17b3aef8f227535f05bb62fead94ef1f7
mahiv87 <maherreraiv1987@gmail.com>
2024-07-10 19:12:35 -0600

    copied tab style from userspage

--- src/layouts/MessagesPage/MessagesPage.tsx
+++ src/layouts/MessagesPage/MessagesPage.tsx
@@ -1,7 +1,38 @@
+import { useState } from 'react';
+
 export const MessagesPage = () => {
+	const [messagesClick, setMessagesClick] = useState(false);
+
 	return (
-		<div>
-			<div>MessagesPage</div>
+		<div className="container mx-auto">
+			<div className="mt-3">
+				<div role="tablist" className="tabs tabs-lifted tabs-lg">
+					<input
+						onClick={() => setMessagesClick(false)}
+						type="radio"
+						name="my_tabs_1"
+						role="tab"
+						className="tab font-semibold text-main-color [--tab-bg:white] [--tab-border:2px] [--tab-border-color:lightgray]"
+						aria-label="Loans"
+						defaultChecked
+					/>
+					<div role="tabpanel" className="tab-content p-10 ">
+						Submit Question
+					</div>
+
+					<input
+						onClick={() => setMessagesClick(true)}
+						type="radio"
+						name="my_tabs_1"
+						role="tab"
+						className="tab font-semibold text-main-color [--tab-bg:white] [--tab-border:2px] [--tab-border-color:lightgray]"
+						aria-label="History"
+					/>
+					<div role="tabpanel" className="tab-content p-10">
+						Pending
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
