0a558f2a8c88df436682f07e1ba0a7f2db80fd92
mahiv87 <maherreraiv1987@gmail.com>
2024-07-16 19:38:19 -0600

    import fetchBooks useEffect

--- src/layouts/ManageLibraryPage/components/ChangeBooksQty.tsx
+++ src/layouts/ManageLibraryPage/components/ChangeBooksQty.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BookModel } from '../../../models/BookModel';
+import Spinner from '../../utils/Spinner';
 
 const ChangeBooksQty = () => {
 	const [books, setBooks] = useState<BookModel[]>([]);
@@ -9,6 +10,56 @@ const ChangeBooksQty = () => {
 	const [booksPerPage] = useState(5);
 	const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
 	const [totalPages, setTotalPages] = useState(0);
+
+	useEffect(() => {
+		const fetchBooks = async () => {
+			const url: string = `http://localhost:8080/api/books?page=${
+				currentPage - 1
+			}&size=${booksPerPage}`;
+
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error('Something went wrong...');
+			}
+
+			const responseJson = await response.json();
+			const responseData = responseJson._embedded.books;
+
+			setTotalAmountOfBooks(responseJson.page.totalElements);
+			setTotalPages(responseJson.page.totalPages);
+
+			const loadedBooks = responseData.map((book: BookModel) => ({
+				id: book.id,
+				title: book.title,
+				author: book.author,
+				description: book.description,
+				copies: book.copies,
+				copiesAvailable: book.copiesAvailable,
+				category: book.category,
+				img: book.img
+			}));
+
+			setBooks(loadedBooks);
+			setIsLoading(false);
+		};
+		fetchBooks().catch((error) => {
+			setIsLoading(false);
+			setHttpError(error.message);
+		});
+		window.scrollTo(0, 0);
+	}, [currentPage]);
+
+	if (isLoading) {
+		return <Spinner />;
+	}
+
+	if (httpError) {
+		return (
+			<div className="container mx-auto my-5" style={{ height: 550 }}>
+				<p>{httpError}</p>
+			</div>
+		);
+	}
 
 	return <div>ChangeBooksQty</div>;
 };
