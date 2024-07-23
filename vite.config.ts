8df97302197627395d3d2eb7cc6972c30b4e77c3
mahiv87 <maherreraiv1987@gmail.com>
2024-07-22 21:01:30 -0600

    removed local ssl

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		https: {
-			key: './ssl-localhost/localhost.key',
-			cert: './ssl-localhost/localhost.crt'
-		}
-	}
+	plugins: [react()]
 });
